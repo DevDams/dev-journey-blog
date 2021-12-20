7295510c2040229898d6485c45c43d8317164eee
DevDams <ad.aimedesire@gmail.com>
2021-12-20 16:27:01 +0000

    update blog name

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,9 +4,9 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'dev-journey',
+    title: 'Dev journey',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'fr'
     },
     meta: [
       { charset: 'utf-8' },
