5edc553596c954f5c25a3d5e1737b5bb93c5a57b
DevDams <ad.aimedesire@gmail.com>
2021-12-30 10:47:12 +0000

    Add readex font

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,4 +11,9 @@ export default {}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Readex+Pro:wght@300;400;500;600;700&display=swap');
+
+body {
+  font-family: 'Readex Pro', sans-serif;
+}
 </style>
